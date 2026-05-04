077fe0028b840f8618d9f076e5b104a6ed4cabfb
Sagnik S <sagnik.cal@gmail.com>
2026-05-04 17:39:17 +0530

    API for uploading JD

--- Backend/controller/jobController.js
+++ Backend/controller/jobController.js
@@ -1,6 +1,9 @@
 // uploadJD
 // parseJD
 
+import mongoose from "mongoose";
+import { jd } from "../models/JDModel.js";
+
 // API to upload job description
 const uploadJD = async(req,res)=>{
     try {
@@ -8,6 +11,10 @@ const uploadJD = async(req,res)=>{
         if(!jdData)
             return res.json({success:false,message:"Please provide a valid job description!!"});
         
+        const data = await jd.create({
+            desc:jdData
+        });
+        return res.json({success:true,message:"JD uploaded"});
     } catch (error) {
         console.log(error);
         return res.json({success:false,message:error.message});
