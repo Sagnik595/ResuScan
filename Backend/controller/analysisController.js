b5539e8460201355488256ff2c823741997766a4
Sagnik S <sagnik.cal@gmail.com>
2026-05-05 18:14:18 +0530

    Added missing skills API

--- Backend/controller/analysisController.js
+++ Backend/controller/analysisController.js
@@ -23,11 +23,34 @@ const analyze1 = async(req,res)=>{
             score
         });
         await anaData.save();
-        return res.json({success:true,message:"Analysis successful",score});
+        return res.json({success:true,message:"Analysis successful",score,id:anaData._id});
     } catch (error) {
         console.log();
         return res.json({success:false,message:error.message});
     }
 }
 
-export {analyze1};
+
+//API to get the missing skills
+const analyze2 = async(req,res)=>{
+    try {
+        const {aid} = req.body;
+        const mainData = await analyze.findById(aid);
+        if(!mainData)
+            return res.json({success:false,message:"No Analytics found!!"});
+        const jData = await jd.findById(mainData.jobId);
+        const rData = await Resume.findById(mainData.resumeId);
+        if(!jData || !rData)
+            return res.json({success:false,message:"Missing job ID or resume ID!!"});
+        const jskills = jData.skills;
+        const rskills = rData.skills;
+        const missing = jskills.filter(item => !rskills.includes(item));
+        mainData.missingSkills = missing;
+        await mainData.save();
+        return res.json({success:true,message:"Missing Skills found",missing});
+    } catch (error) {
+        console.log(error);
+        return res.json({success:false,message:error.message});
+    }
+}
+export {analyze1, analyze2};
--- Backend/routes/analyticsRoute.js
+++ Backend/routes/analyticsRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { analyze1 } from '../controller/analysisController.js';
+import { analyze1, analyze2 } from '../controller/analysisController.js';
 
 const AnalyzeRouter = express.Router();
 
 AnalyzeRouter.post('/analyze',analyze1);
+AnalyzeRouter.post('/analyzeMissing',analyze2);
 
 export default AnalyzeRouter;
